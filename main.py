1743c20e527d24171536fcd1df8847c9a5447c5c
Teaboyyyyy <96498782+Teaboyyyyy@users.noreply.github.com>
2022-06-30 20:44:01 +0000

    Blep added prefixes

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import discord
 import os
-
+from discord.ext import commands 
 client = discord.Client()
+
+client = commands.Bot(command_prefix = '$')
 
 
 @client.event
 async def blop(message):
-  if message.author != client.user and message.content.lower() == "blep":
-    blop = "blop"
-    await message.channel.send(blop)
+    if message.author != client.user:
+        await message.channel.send("blop")
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('blep'):
+        await message.channel.send('blop')
+
+
+@client.command()
+async def blep(ctx):
+  await ctx.send('Blup')
+  
 
 
 my_secret = os.environ['bot_secret']
 client.run(my_secret)
+#DO NOT PUT THE CODE AFTER HERE
